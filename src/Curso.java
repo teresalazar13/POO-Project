a89c588a50254ab518e3c806a4b6e2b6cc4ccc69
teresalazar13 <teresa.sal13@gmail.com>
2016-11-14 11:42:26 +0000

    Adds Arraylists.

--- src/Curso.java
+++ src/Curso.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Curso {
     private String nome;
+    private int duracao;
     private String grau;
+    private ArrayList<Disciplina> disciplinas;
 
     public Curso() {};
 
-    public Curso(String nome, String grau) {
+    public Curso(String nome, int duracao, String grau, ArrayList<Disciplina> disciplinas) {
         this.nome = nome;
+        this.duracao = duracao;
         this.grau = grau;
+        this.disciplinas = disciplinas;
     }
 
     public String getNome() {
@@ -19,11 +24,27 @@ public class Curso {
         this.nome = nome;
     }
 
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
     public String getGrau() {
         return grau;
     }
 
     public void setGrau(String grau) {
         this.grau = grau;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(ArrayList<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
     }
 }
--- src/Projeto.java
+++ src/Projeto.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Projeto {
 
     private static ArrayList<Exame> exames = new ArrayList<Exame>();
+    private static ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+    private static ArrayList<Docente> docentes = new ArrayList<Docente>();
+    private static ArrayList<NaoDocente> naoDocentes = new ArrayList<NaoDocente>();
+    private static ArrayList<Curso> cursos = new ArrayList<Curso>();
+    private static ArrayList<Disciplina> disciplinas = new ArrayList<Disciplina>();
 
     public static void adicionarExame() {
         Exame exame = new Exame();
@@ -20,6 +25,12 @@ public class Projeto {
     }
 
     public static void menu() {
+        Curso curso = new Curso("Engenharia Informatica", 3, "Licenciatura", disciplinas);
+        Aluno aluno = new Aluno("Teresa", "teresa.sal13@gmail.com", 20, 1, curso, "normal");
+        Docente docente = new Docente("Marilia Curado", "mariliacurado@dei.uc.pt", 1, "Catedratico", "Engenharia de Software");
+        NaoDocente naoDocente = new NaoDocente("Jorge", "jorge@gmail.com", 10, "Tecnico", "Secretaria");
+        Disciplina disciplina = new Disciplina("POO", docente, docentes, alunos);
+        Data data = new Data(1,2,3,4,5);
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.println("Pretende\n" +
